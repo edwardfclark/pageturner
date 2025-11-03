ebd5bda7fc899cb3b788c0a81276d1c66cee6740
Edward Clark <edwardfclark@gmail.com>
2025-11-03 16:35:09 +0000

    Implement sign-up and sign-out

---
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,9 @@
+import { SignIn } from "@clerk/nextjs";
+
+export default function SignInPage() {
+  return (
+    <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
+      <SignIn signUpUrl="/sign-up" />
+    </div>
+  );
+}
---
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,9 @@
+import { SignUp } from "@clerk/nextjs";
+
+export default function SignUpPage() {
+  return (
+    <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
+      <SignUp signInUrl="/sign-in" />
+    </div>
+  );
+}
